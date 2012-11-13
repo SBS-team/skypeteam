c3deb30f59f2ca309880f07b91f8e882ad052c4f
Max Dolgih <maxim.dolgih@tech-angels.com>
2012-11-13 14:41:58 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skypeteam::Application.config.secret_token = '3a93a4c276422f941dc7d3712bf6f8d4eaa9e7aadb63895625f32f27d3ec7fc37a3f6094d990d4cfa55d1138d7987fe9628e4c8d1741d0bce705b75c91ecb31a'
